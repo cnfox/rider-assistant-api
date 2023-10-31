8b54cf9d8e97b02473c707f05f849eee1a280ec3
zane <pythonup@hotmail.com>
2023-10-31 15:21:16 +0800

    精简

--- config/message.go
+++ config/message.go
@@ -1,7 +1,5 @@
 package config
 
-import "os"
-
 type EmailConfig struct {
 	Host     string
 	Port     int
@@ -10,65 +8,11 @@ type EmailConfig struct {
 }
 
 var MailConfig EmailConfig
-
 var AlertEmailList = []string{
 	"pythonup@hotmail.com",
 }
 
-type Kafka struct {
-	Broker []string
-}
-
-var KafkaConfig Kafka
-
-var KafkaTopic map[string]string
-
-type RabbitMQ struct {
-	Host string
-	Port string
-	User string
-	Pass string
-}
-
-var RabbitMQConfig RabbitMQ
-
-var RabbitMQQueue map[string]string
-
 func init() {
-	if os.Getenv("GIN_MODE") == "release" {
-		KafkaConfig = Kafka{
-			[]string{
-				"127.0.0.1:9092",
-			},
-		}
-		RabbitMQConfig = RabbitMQ{
-			"localhost",
-			"5672",
-			"guest",
-			"guest",
-		}
-	} else {
-		KafkaConfig = Kafka{
-			[]string{
-				"127.0.0.1:9092",
-			},
-		}
-		RabbitMQConfig = RabbitMQ{
-			"localhost",
-			"5672",
-			"guest",
-			"guest",
-		}
-	}
-
-	KafkaTopic = map[string]string{
-		"topic": "topic",
-	}
-
-	RabbitMQQueue = map[string]string{
-		"queue": "queue",
-	}
-
 	MailConfig = EmailConfig{
 		"smtp.163.com",
 		465,

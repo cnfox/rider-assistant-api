6515bd3cf658c7d18e71701b6a20c9ab85cc09a5
zane <pythonup@hotmail.com>
2023-10-31 15:23:08 +0800

    注释

--- main.go
+++ main.go
@@ -12,10 +12,6 @@ var (
 	SetCpuCount string
 )
 
-// @title go-skeleton
-// @version 1.0
-// @description Golang 脚手架，Go 简单，Go 直接
-// @termsOfService https://github.com/armnerd/go-skeleton
 func main() {
 	if SetCpuCount != "" {
 		procsNum, err := strconv.Atoi(SetCpuCount)
